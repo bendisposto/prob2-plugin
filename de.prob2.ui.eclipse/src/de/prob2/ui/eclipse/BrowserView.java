8e9c990e96551db3e743a135c35a25eaf0cb1970
Jens Bendisposto <jens@bendisposto.de>
2014-12-08 16:17:39 +0100

    fixed refresh problem

--- de.prob2.ui.eclipse/src/de/prob2/ui/eclipse/BrowserView.java
+++ de.prob2.ui.eclipse/src/de/prob2/ui/eclipse/BrowserView.java
@@ -2,9 +2,14 @@ package de.prob2.ui.eclipse;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.browser.ProgressAdapter;
+import org.eclipse.swt.browser.ProgressEvent;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
 
+import de.prob.Main;
+import de.prob.cli.CliVersionNumber;
+import de.prob.scripting.Api;
 import de.prob.webconsole.WebConsole;
 
 public class BrowserView extends ViewPart {
@@ -17,6 +22,8 @@ public class BrowserView extends ViewPart {
 	public BrowserView(String url) {
 		this.url = url;
 		port = WebConsole.getPort();
+
+
 	}
 
 	/**
@@ -31,27 +38,38 @@ public class BrowserView extends ViewPart {
 	private void createSWTBrowser(Composite parent) {
 		Browser b = new Browser(parent, SWT.NONE);
 		this.browser = b;
+
 		String u = getUrl();
 		if (u != null && !u.isEmpty())
 			load(u);
 		canvas = b;
 	}
 
-//	public void refresh() {
-//		browser.refresh();
-//	}
+	// public void refresh() {
+	// browser.refresh();
+	// }
 
 	public void load(String url) {
 		if (url == null || url.isEmpty()) {
 			// FIXME log error?
 			return;
 		}
+		
+		browser.addProgressListener(new ProgressAdapter() {
+			public void completed(ProgressEvent event) {
+				browser.removeProgressListener(this);
+				browser.refresh(); // <----
+			}
+		});
 		if (url.startsWith("http://")) {
 			browser.setUrl(url);
 		} else {
-			browser.setUrl("http://localhost:" + port + "/" + url);
+			String theUrl = "http://localhost:" + port + "/" + url;
+			System.out.println("Loading: ###" + theUrl + "###");
+			browser.setUrl(theUrl);
 		}
 		VersionController.ensureInstalled();
+
 	}
 
 	protected String getUrl() {
